c92a9f380be3f273a7978a4fc142e53e3427e99a
nayayaaa <122560595+nayayaaa@users.noreply.github.com>
2023-01-31 13:31:19 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,26 +9,59 @@ function updateScoreboard () {
     OLED.writeStringNewLine("Rounds: " + Total_Rounds)
 }
 input.onButtonPressed(Button.A, function () {
-	
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    basic.clearScreen()
+    Total_Rounds += 1
+    PA += 1
+    updateScoreboard()
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # # . .
+        `)
+    basic.clearScreen()
+    Total_Rounds += 1
+    Draw += 1
+    updateScoreboard()
 })
 input.onButtonPressed(Button.B, function () {
-	
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . # .
+        . # # . .
+        `)
+    basic.clearScreen()
+    Total_Rounds += 1
+    PB += 1
+    updateScoreboard()
 })
 input.onGesture(Gesture.Shake, function () {
-	
+    reset()
 })
+function reset () {
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("wanna play a game?")
+    PA = 0
+    PB = 0
+    Draw = 0
+    Total_Rounds = 0
+    basic.pause(2000)
+    updateScoreboard()
+}
 let Total_Rounds = 0
 let Draw = 0
 let PB = 0
 let PA = 0
-OLED.init(128, 64)
-OLED.writeStringNewLine("wanna play a game?")
-PA = 0
-PB = 0
-Draw = 0
-Total_Rounds = 0
-basic.pause(2000)
-updateScoreboard()
+reset()
